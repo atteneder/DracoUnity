5f56dcf6efc71eb6a9f6138a2a0a964a0b036182
Andreas Atteneder <andreas.atteneder@gmail.com>
2019-06-30 18:09:47 +0200

    Integrated fix from (unmerged) PR https://github.com/google/draco/pull/493

--- Runtime/Scripts/DracoMeshLoader.cs
+++ Runtime/Scripts/DracoMeshLoader.cs
@@ -47,6 +47,8 @@ public unsafe class DracoMeshLoader
 	[DllImport ("dracodec_unity")] private static extern int DecodeMeshForUnity (
 		byte[] buffer, int length, DracoToUnityMesh**tmpMesh);
 
+	[DllImport("dracodec_unity")] private static extern int ReleaseUnityMesh(DracoToUnityMesh** tmpMesh);
+
 	static private int maxNumVerticesPerMesh = 60000;
 
 	// Unity only support maximum 65534 vertices per mesh. So large meshes need
